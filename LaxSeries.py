90ff96902071d958950a9320cbdb4d30d48b043f
Joey Dumont <joey.dumont@gmail.com>
2018-08-17 16:26:08 -0400

    Some SymPy tests.

--- LaxSeries.py
+++ LaxSeries.py
@@ -14,10 +14,12 @@ import matplotlib.pyplot as plt
 import scipy.special as sp
 import scipy.interpolate as interpolate
 import scipy.integrate as integrate
+import scipy.constants as cst
 import itertools
 import unittest
+import sympy
 
-# -------------------------------- Functions  ------------------------------- #
+# -------------------------------- Functions -------------------------------- #
 
 def ExpansionCoefficient(m,p):
   """
@@ -54,14 +56,24 @@ def ExpansionCoefficientDirect(m,p):
   return result
 
 
-# -- Tests
+# ----------------------------- SymPy Declaration --------------------------- #
+x, y, z,k, w_0 = sympy.symbols('x y z k w_0')
+z_r     = k*w_0**2/2
+w_z     = w_0*sympy.sqrt(1+(z/z_r)**2)
+phi_0   = w_0/w_z*sympy.exp(-(x**2+y**2)/w_z**2)*sympy.exp(-1j*k*z+1j*sympy.atan(z/z_r))
+
+
+# ------------------------------- Unit Testing ------------------------------ #
 class TestExpansionCoefficients(unittest.TestCase):
 
   def test_both_functions(self):
+    """
+    Test that both implementations agree for small m and p.
+    """
     m_l = [i for i in range(1,15)]
     p_l = [i for i in range(1,15)]
 
-    for iter in itertools.product(m_l,p_l):
+    for iter in itertools.filterfalse(lambda x : x[0] < x[1], itertools.product(m_l,p_l)):
       a = ExpansionCoefficient(*iter)
       b = ExpansionCoefficientDirect(*iter)
       print(*iter, a)
@@ -71,6 +83,26 @@ class TestExpansionCoefficients(unittest.TestCase):
 if __name__ == "__main__":
 
   # -- Run tests.
-  unittest.main()
+  #unittest.main()
 
+  # -- Substitute numerical values for fixed parameters in phi_0.
+  lamb  = 800e-9
+
+  phi_0 = phi_0.subs(k, 2*sympy.pi/lamb)
+  phi_0 = phi_0.subs(w_0,lamb)
+
+  phi = sympy.lambdify((x,y,z), phi_0)
+
+  x_f = np.linspace(-1.5*lamb,1.5*lamb,100)
+  y_f = np.linspace(-1.5*lamb,1.5*lamb,100)
+  X, Y = np.meshgrid(x_f,y_f)
+  z   = 0
+
+  print(phi_0)
+  print("test")
+  print(phi(X,Y,z))
+
+  plt.figure()
+  plt.pcolormesh(X,Y,np.abs(phi(X,Y,z)))
+  plt.show()
 
